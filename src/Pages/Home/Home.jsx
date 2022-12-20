854246b5d5da32c850ac0e4a108e2392e446b10b
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-12-21 02:03:57 +0600

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const Home = () => {
     return (
-        <div>
-            home
-        </div>
+        <Container className='mt-4'>
+            <Row>
+                <Col md={4}>
+                    home
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
