66bd1dc423a3f312206b28b2663f56a9788d6b27
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-12-21 20:27:12 +0600

    make live

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,5 +1,4 @@
 import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
 import React, { createContext, useEffect, useState } from 'react';
 
 export const AuthContext = createContext();
